4578ac955b5543e9a35142d12074cc901f4bfc9b
rxzkie <renzo_renzo_2001@hotmail.com>
2022-06-24 20:26:00 -0400

    despliegue de alerta
    se agrego el metodo de alerta al ts de evento

--- src/app/evento/evento.page.ts
+++ src/app/evento/evento.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { FirestoreService } from '../services/firestore.service';
 
 @Component({
@@ -8,18 +9,43 @@ import { FirestoreService } from '../services/firestore.service';
 })
 export class EventoPage implements OnInit {
 
-  constructor(public firestore:FirestoreService) { }
+  constructor(
+    public firestore: FirestoreService,
+    public alertController : AlertController
+
+  ) { }
 
   ngOnInit() {
   }
 
-  guardarEvento(){
-    const data={
-      nombre:'prueba',
-      precio:40
+  guardarEvento() {
+    const data = {
+      nombre: 'prueba',
+      precio: 40
     };
-    const path='evento/';
+    const path = 'evento/';
     const id = '001'
-    this.firestore.createDoc(data,path,id)
+    this.firestore.createDoc(data, path, id)
   }
+
+
+  async presentAlert1() {
+    const alert = await this.alertController.create({
+
+      header: "Enviado",
+      message: "gracias por enviar",
+      buttons: ["ok"],
+
+    });
+    await alert.present()
+
+    let result = await alert.onDidDismiss();
+
+    console.log(result)
+
+  }
+
+
+
+
 }
